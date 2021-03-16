2057d12b3338a0fc2d207b3a6ccb6f981f61af2a
wellbell <uok02018496@naver.com>
2021-03-17 01:12:03 +0900

    프로그래머스 전화번호 목록 문제

--- src/programmers/PhoneBook.java
+++ src/programmers/PhoneBook.java
@@ -3,20 +3,14 @@ package programmers;
 import java.util.Arrays;
 
 // 프로그래머스 전화번호 목록 문제
-class PhoneBook {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            String target = phone_book[i];
-
-            for(int j = i + 1; j < phone_book.length; j++) {
-                if(phone_book[j].startsWith(target)) {
-                    answer = false;
-                    break;
-                }
+class Solution {
+    static public boolean solution(String[] phoneBook) {
+        Arrays.sort(phoneBook);
+        for(int i=0; i<phoneBook.length-1;i++){
+            if(phoneBook[i+1].startsWith(phoneBook[i])) {
+                return false;
             }
         }
-        return answer;
+        return true;
     }
 }
