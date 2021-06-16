91d56327b549e67d2825e2f1b99be9dce0f5bc71
wellbell <uok02018496@naver.com>
2021-06-16 19:30:01 +0900

    프로그래머스 내적 문제

---
+++ src/programmers/Inner.java
@@ -0,0 +1,12 @@
+package programmers;
+
+// 프로그래머스 내적 문제
+class Inner {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
